8fd0b193bc9ab6f7eba25e599f9a4676d87f17f6
SaroshKhawar496 <sarosh.496@gmail.com>
2019-03-25 17:59:01 -0400

    Added actions for auth and error. Created auth and error reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,55 @@
+import {
+    USER_LOADING,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    LOGOUT_SUCCESS,
+    REGISTER_SUCCESS,
+    REGISTER_FAIL   
+} from '../actions/types';
+import { stat } from 'fs';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    isLoading: false,
+    user: null
+};
+
+export default function(state = initialState, action){
+    switch(action.type){
+        case USER_LOADING:
+            return{
+                ...state,
+                isLoading: true
+            };
+        case USER_LOADED: //runs with every request to see if we are logged in or not
+            return{
+                isAuthenticated: true,
+                isLoading: false,
+                user: action.payload
+            };
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS: 
+            return{
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                isLoading: false
+            };
+        case LOGIN_FAIL:
+        case REGISTER_FAIL: 
+        case AUTH_ERROR: 
+        case LOGOUT_SUCCESS:
+            return{
+                ...state,
+                token: null,
+                user: null,
+                isAuthenticated: false,
+                isLoading: false
+            }
+        default: 
+            return state;
+    }
+}
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,12 @@
 // to bring together all other reducers in the app. this is rootReducer
 import {combineReducers} from 'redux';
 import itemReducer from './itemReducer';
+import authReducer from './authReducer';
+import errorReducer from './errorReducer';
 
 export default combineReducers({
     item: itemReducer,
     // other reducers
+    auth: authReducer,
+    error: errorReducer
 });

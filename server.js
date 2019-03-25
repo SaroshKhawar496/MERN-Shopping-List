4c1f964790c3c0cb2fa672d124d1f4a4a0d28929
SaroshKhawar496 <sarosh.496@gmail.com>
2019-03-24 21:40:36 -0400

    Added routes for user registration. User registration logic works

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+
 const path = require("path");
 
 const items = require('./routes/api/items');
-
+const users = require('./routes/api/users');
 
 const app = express();
 
 //Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
-mongoose.connect(db,{useNewUrlParser:true})
+mongoose.connect(db,{useNewUrlParser:true, useCreateIndex: true})
     .then(
         ()=>console.log("MongoDB Connected")
     )
@@ -23,6 +23,7 @@ mongoose.connect(db,{useNewUrlParser:true})
 
 // Use Routes
 app.use('/api/items/', items);
+app.use('/api/users/', users);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
